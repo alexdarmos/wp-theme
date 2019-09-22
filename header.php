35e6d7aa42a65ea78c4c0cfdfb31fb68780ca09e
Alex Darmos <alex.darmos@gmail.com>
2019-09-22 18:12:57 -0400

    nav, home page and other pages basic setup

--- header.php
+++ header.php
@@ -6,4 +6,18 @@
 
     </head>
     <body <?php body_class();?> >
+
+    <header class="sticky-top">
+
+    <div class="container">
+        <?php wp_nav_menu(
+            array (
+                'theme_location' => 'top-menu',
+                'menu_class' => 'navigation'
+
+            )
+            );?>
+            </div>
+    
+    </header>
   
